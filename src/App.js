95edfd337b74368284481fdced8461cad2b1e2fd
zleodai <awsomeleodai@gmail.com>
2022-11-21 11:37:42 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  SignIn,
+  SignOut,
+  useAuthentication,
+} from "../src/services/authService";
 
-function App() {
+export default function App() {
+  const user = useAuthentication();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{!user ? <SignIn /> : <SignOut />}</header>
     </div>
   );
 }
-
-export default App;

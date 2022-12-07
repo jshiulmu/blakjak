ce35dbd12a520a17fc4b8ff674a1b257598c8ac5
jshiulmu <jshiuwork@gmail.com>
2022-12-07 11:47:04 -0800

    combined commits

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,52 @@
 import { getDefaultEmulatorHost } from '@firebase/util'
+import { disableNetwork } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 import { SignIn, SignOut, useAuthentication } from '../src/services/authService'
-import App from './App.js'
+import App from './App'
 
 export default function Game(user) {
     const [playing, setPlaying] = useState(true)
-    const [playerHand, setPlayerHand] = useState(0)
-    const [dealerHand, setDealerHand] = useState(0)
+    const [playerHand, setPlayerHand] = useState([])
+    const [dealerHand, setDealerHand] = useState([])
     const [gameOver, setGameOver] = useState(false)
+    const [deckID, setdeckID] = useState('')
+    function fetchDeck() {
+        const requestURL =
+            'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+        fetch(requestURL)
+            .then((response) => {
+                if (response.status < 400 && response.status >= 200) {
+                    return response.json()
+                } else {
+                    console.log(response.status)
+                    throw new Error()
+                }
+            })
+            .then((deck) => setdeckID(deck.deck_id))
+            .catch(() => setdeckID(null))
+        console.log(deckID)
+        initialDeal()
+    }
+    function initialDeal() {
+        const base = ''
+        const requestURL = base.concat(
+            'https://www.deckofcardsapi.com/api/deck/',
+            deckID,
+            '/draw/?count=2'
+        )
+        fetch(requestURL)
+            .then((response) => {
+                if (response.status < 400 && response.status >= 200) {
+                    return response.json()
+                } else {
+                    console.log(response.status)
+                    throw new Error()
+                }
+            })
+            .then((cardsList) => setPlayerHand(cardsList.cards))
+            .catch(() => setdeckID(null))
+        console.log(playerHand)
+    }
 
     function playerHit() {
         setPlayerHand(playerHand + Math.random() * 10) //CHANGE TO API CALL
@@ -72,12 +111,11 @@ export default function Game(user) {
                     onClick={() => {
                         console.log('EXITING')
                         setPlaying(false)
-                    }}>
+                    }}
+                >
                     Return to Menu
                 </button>
-                <div className="PlayerInfo">
-                    Player Hand:
-                </div>
+                <div className="PlayerInfo">Player Hand:</div>
             </div>
         )
     ) : (

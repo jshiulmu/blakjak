d3ddb671f92d0e32a6aa8bafbd8f0c708c90ccda
jshiulmu <jshiuwork@gmail.com>
2022-12-07 11:21:57 -0800

    api call for deck_id

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 import { getDefaultEmulatorHost } from '@firebase/util'
+import { disableNetwork } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 import { SignIn, SignOut, useAuthentication } from '../src/services/authService'
 import App from './App'
@@ -8,6 +9,23 @@ export default function Game(user) {
     const [playerHand, setPlayerHand] = useState(0)
     const [dealerHand, setDealerHand] = useState(0)
     const [gameOver, setGameOver] = useState(false)
+    const [deckID, setdeckID] = useState('')
+    function fetchDeck() {
+        const requestURL =
+            'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+        fetch(requestURL)
+            .then((response) => {
+                if (response.status < 400 && response.status >= 200) {
+                    return response.json()
+                } else {
+                    console.log(response.status)
+                    throw new Error()
+                }
+            })
+            .then((deck) => setdeckID(deck.deck_id))
+            .catch(() => setdeckID(null))
+        console.log(deckID)
+    }
 
     function playerHit() {
         setPlayerHand(playerHand + Math.random() * 10) //CHANGE TO API CALL
@@ -68,15 +86,14 @@ export default function Game(user) {
                         </button>
                     </div>
                     <div>
-                        <button className="stand_button" onClick={stand}>
-                            Stand
+                        <button className="CHECK_API" onClick={fetchDeck}>
+                            CHECK API
                         </button>
                     </div>
                     <div>
-                        <button className="button">Settings</button>
-                    </div>
-                    <div>
-                        <button className="button">Leaderboards</button>
+                        <button className="stand_button" onClick={stand}>
+                            Stand
+                        </button>
                     </div>
                     <div>
                         <button
